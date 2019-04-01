2d5dc27c98e930b9c9907e5d5520d5e75a0c594a
Poitier Stringer <pstringe@e1z3r2p4.42.us.org>
2019-04-01 13:21:40 -0700

    ex01 works

--- d04/ex01/bit.c
+++ d04/ex01/bit.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 12:56:41 by pstringe          #+#    #+#             */
-/*   Updated: 2019/04/01 13:15:14 by pstringe         ###   ########.fr       */
+/*   Updated: 2019/04/01 13:17:33 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,11 +55,20 @@ char *getOr(char *a, char *b)
 {
 	char	*r;
 	int		i;
+	int 	anot;
+	int 	bnot;
 
 	r = malloc(5);
 	bzero(r, 5);
 	i = 4;
+
+	anot = a[0]  == '~' ? 1 : 0;
+	bnot = b[0]  == '~' ? 1 : 0;
+	a = anot ? a + 1 : a;
+	b = bnot ? b + 1 : b;
 	while(--i >= 0)
-		 r[i] = (a[i] - '0') || (b[i] - '0') ? '1' : '0';
+		 r[i] = (anot ? !(a[i] - '0') : a[i] - '0') || 
+			 	(bnot ? !(b[i] - '0') : b[i] - '0') 
+				? '1' : '0';
 	return (r);
 }
